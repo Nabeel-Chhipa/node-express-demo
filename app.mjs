ad1c73915e39bc1d64c01a1b09977aeef4a1ec30
Nabeel-Chhipa <nabeelchipa@gmail.com>
2022-10-26 02:03:05 +0500

    add basic routes

--- app.mjs
+++ app.mjs
@@ -1,7 +1,9 @@
 import express from "express";
 
 const app = express()
-const port = 5000
+const port = 3000
 
-
-app.listen(port, () => console.log('Server Start'))
+app.use('/login', (req, res) => res.send('Login'))
+app.use('/Register', (req, res) => res.send('Register'))
+app.use('/admin', (req, res) => res.send('admin'))
+app.listen(port, () => console.log(`Server Start on port ${port}`))
